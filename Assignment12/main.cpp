55341614d89300f78d5cc0361e0fb25f3d2c0b33
ViRack <vincentm_2@hotmail.com>
2021-11-21 13:07:57 -0800

    Options 1 and 2 completed, not yet checked for bugs

--- Assignment12/main.cpp
+++ Assignment12/main.cpp
@@ -52,19 +52,22 @@ void mainMenu(void)
 //PostCondition: 
 void programOne(void)
 {
+    DynamicString dynamicString;
+
     do
     {
         displayOptionOneMenu();
         int option = inputChar("\t\tOption: ");
 
+
         switch (option)
         {
         case '0': return;
-        case 'a': case 'A': /*function here*/ break;
-        case 'b': case 'B': /*function here*/ break;
-        case 'c': case 'C': /*function here*/ break;
-        case 'd': case 'D': /*function here*/ break;
-        case 'e': case 'E': /*function here*/ break;
+        case 'a': case 'A': A(dynamicString); break;
+        case 'b': case 'B': B(dynamicString); break;
+        case 'c': case 'C': C(dynamicString); break;
+        case 'd': case 'D': D(dynamicString); break;
+        case 'e': case 'E': E(dynamicString); break;
         default: cout << "\t\tERROR-1A: Invalid input. Must be '0','A','B','C','D', or 'E'" << endl;
             pause("\n\t\tPress enter to continue...");
         }
@@ -76,6 +79,8 @@ void programOne(void)
 //PostCondition: 
 void programTwo(void)
 {
+    DynamicString dynamicString;
+
     do
     {
         displayOptionTwoMenu();
@@ -85,11 +90,11 @@ void programTwo(void)
         switch (option)
         {
         case '0': return;
-        case 'a': case 'A': /*function here*/ break;
-        case 'b': case 'B': /*function here*/ break;
-        case 'c': case 'C': /*function here*/ break;
-        case 'd': case 'D': /*function here*/ break;
-        case 'e': case 'E': /*function here*/ break;
+        case 'a': case 'A': A2(dynamicString); break;
+        case 'b': case 'B': B2(dynamicString); break;
+        case 'c': case 'C': C2(dynamicString); break;
+        case 'd': case 'D': D2(dynamicString); break;
+        case 'e': case 'E': E2(dynamicString); break;
         default: cout << "\t\tERROR-1A: Invalid input. Must be '0','A','B','C','D', or 'E'" << endl;
             pause("\n\t\tPress enter to continue...");
         }
